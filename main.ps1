d78492d28e2c4f8df357c41d15c76d7162588e8c
Blueturned <Jelgerpopken@Gmail.com>
2025-11-20 16:48:04 +0100

    Main changes

--- main.ps1
+++ main.ps1
@@ -1,3 +1,17 @@
-$config = './Config.ps1'
+$config = & .\config.ps1
 
-Write-Host $config.ModuleSucces
+if (-not $config.ModuleSucces) {
+    Write-Error "Config failed, exiting script"
+    exit 1
+}
+
+$ftpServer = "192.168.2.69"
+
+$cred = Get-Credential
+$sessionOption = New-WinSCPSessionOption -HostName $ftpServer -Protocol ftp -Credential $cred
+
+New-WinSCPSession -SessionOption $sessionOption
+
+New-WinSCPItem -Path '/home/blueturned/Documents/test' -ItemType Directory
+
+Remove-WinSCPSession
